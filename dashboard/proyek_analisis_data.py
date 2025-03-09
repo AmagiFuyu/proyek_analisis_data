2df6384337dfc7e06f99fac870ab0d389c3fe525
AmagiFuyu <114065239+AmagiFuyu@users.noreply.github.com>
2025-03-09 10:46:28 +0700

    revisi

--- dashboard/proyek_analisis_data.py
+++ dashboard/proyek_analisis_data.py
@@ -8,10 +8,19 @@ day_df = pd.read_csv("/content/day.csv")
 hour_df = pd.read_csv("/content/hour.csv")
 
 # Convert date column
-day_df["dteday"] = pd.to_datetime(day_df["dteday"], format='%d/%m/%Y', dayfirst=True)
-hour_df["dteday"] = pd.to_datetime(hour_df["dteday"], format='%d/%m/%Y', dayfirst=True)
+day_df["dteday"] = pd.to_datetime(day_df["dteday"], format='%d/%m/%Y')
+hour_df["dteday"] = pd.to_datetime(hour_df["dteday"], format='%d/%m/%Y')
 
-st.title("Analisis Peminjaman Sepeda")
+st.title("Dashboard Peminjaman Sepeda 🚴🏻‍♀️")
+# Sidebar Date Filter
+start_date = st.sidebar.date_input("Start Date", day_df["dteday"].min())
+end_date = st.sidebar.date_input("End Date", day_df["dteday"].max())
+
+# Filter data based on date selection
+filtered_df = day_df[(day_df["dteday"] >= pd.to_datetime(start_date)) & (day_df["dteday"] <= pd.to_datetime(end_date))]
+
+# Display filtered data
+st.write("### Filtered Data Preview", filtered_df.head())
 
 # Monthly Trend
 st.subheader("Tren Peminjaman Sepeda Sepanjang Tahun")
@@ -35,5 +44,31 @@ ax.set_xlabel("Jam dalam Sehari")
 ax.set_ylabel("Rata-rata Jumlah Peminjaman")
 ax.set_title("Pola Peminjaman Sepeda")
 ax.set_xticks(range(0, 24))
+ax.grid()
+st.pyplot(fig)
+
+#Season trend
+st.subheader("Tren Peminjaman Sepeda Berdasarkan Musim")
+season_trend = day_df.groupby("season")["cnt"].mean()
+fig, ax = plt.subplots()
+sns.boxplot(x="season", y="cnt", data=day_df, ax=ax)
+ax.set_xticks([0, 1, 2, 3])
+ax.set_xticklabels(["Spring", "Summer", "Fall", "Winter"])
+ax.set_xlabel("Musim")
+ax.set_ylabel("Jumlah Peminjaman")
+ax.set_title("Distribusi Peminjaman Sepeda Berdasarkan Musim")
+ax.grid()
+st.pyplot(fig)
+
+#Weather trend
+st.subheader("Tren Peminjaman Sepeda Berdasarkan Cuaca")
+weather_trend = day_df.groupby("weathersit")["cnt"].mean()
+fig, ax = plt.subplots()
+sns.boxplot(x="weathersit", y="cnt", data=day_df)
+ax.set_xticks([0, 1, 2, 3])
+ax.set_xticklabels(["Cerah", "Mendung", "Hujan Ringan", "Hujan Lebat"])
+ax.set_xlabel("Kondisi Cuaca")
+ax.set_ylabel("Jumlah Peminjaman")
+ax.set_title("Pengaruh Kondisi Cuaca terhadap Peminjaman Sepeda")
 ax.grid()
 st.pyplot(fig)
